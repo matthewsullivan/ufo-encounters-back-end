b4fe49fe11457c44ea8e24e0d54082ecf9350b29
Matthew Sullivan <contact@matthewsullivan.media>
2019-09-16 18:21:17 -0400

    Add CORS support for local development url

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const Koa = require('koa');
 
 const app = new Koa();
+const convert = require('koa-convert');
+const cors = require('kcors');
 const glob = require('glob');
 const http = require('http');
 const path = require('path');
 
 const config = require(path.resolve('./config/env/default'));
+const origin = process.env.URL_ORIGIN;
+const whitelist = origin ? origin.split(' ').join(' || ') : 'http://localhost:3001';
+
+const corsOptions = {
+  credentials: true,
+  origin: getOrigin,
+};
+
+/**
+ * Get origin
+ * @param {object} ctx
+ * @return {object}
+ */
+function getOrigin(ctx) {
+  const origin = ctx.accept.headers.origin;
+
+  if (!whitelist.includes(origin)) {
+    return ctx.throw(`${origin} is not a valid origin`);
+  }
+
+  return origin;
+}
+
+
+app.use(convert(cors(corsOptions)))
 
 glob.sync('./modules/*/routes/*.js').forEach((file) => { 
   app.use(require(path.resolve(file)).routes());
